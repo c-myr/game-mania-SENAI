cbaf25aa168b324c96b4e301dd7372d2562f06ff
Maya <mayaraa_costaa@outlook.com>
2022-11-08 13:23:34 -0300

    carrossel adicionado

--- Game Mania/js/script.js
+++ Game Mania/js/script.js
@@ -1,3 +1,33 @@
+$(document).ready(function () {
+
+    let slideAtual = 1
+    let listaSlides = ["banner-1", "banner-2", "banner-3"]
+
+    setInterval(mudarSlide, 4000);
+
+    function mudarSlide() {
+        //remover o slider anterior
+        if (slideAtual > 0) {
+            $("#carrossel").removeClass(listaSlides[slideAtual - 1])
+        }
+        else {
+            $("#carrossel").removeClass(listaSlides[listaSlides.length - 1])
+        }
+
+        //exibir o slide atual
+        $("#carrossel").addClass(listaSlides[slideAtual])
+
+        //indicar qual o slide atual
+        console.log("Slide atual é: ", slideAtual)
+        slideAtual++
+
+        if (slideAtual > listaSlides.length - 1) {
+            slideAtual = 0
+        }
+    }
+})
+
+
 function mostrarMenu($event) {
     let menu = document.getElementById("nav")
 
